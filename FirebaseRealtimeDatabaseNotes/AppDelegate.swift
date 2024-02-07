54dd3ff0f384ad698945797718ff9fd15c5cf707
Stanislav <stazilastas@gmail.com>
2024-02-07 09:48:33 -0600

    Getting Firebase account and packages

--- FirebaseRealtimeDatabaseNotes/AppDelegate.swift
+++ FirebaseRealtimeDatabaseNotes/AppDelegate.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import FirebaseCore
+import FirebaseDatabase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
