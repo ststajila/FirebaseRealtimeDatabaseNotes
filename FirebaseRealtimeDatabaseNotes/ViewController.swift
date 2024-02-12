b9c7a2765951082e71575a245d65305e993e63d7
stasik <stazilastas@gmail.com>
2024-02-11 18:06:14 -0600

    Step 4

--- FirebaseRealtimeDatabaseNotes/ViewController.swift
+++ FirebaseRealtimeDatabaseNotes/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        
     }
 
-
+    @IBAction func save(_ sender: Any) {
+        var name = nameTextField.text
+        ref.child("students").childByAutoId().setValue(name)
+    }
+    
 }
 
